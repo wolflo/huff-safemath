c1ef09d55d060d7a62dbd86fe48ca4c1b18480d8
nsward <wardnick@protonmail.com>
2019-05-31 16:08:26 -0600

    test structure

---
+++ huff_tests/main_loop.spec.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const bn = require('bn.js');
+const path = require('path');
+
+const { Runtime } = require('../huff');
+
+const { expect } = chai;
+
+const pathToTestData = path.posix.resolve(__dirname, '../huff_modules');
+
+describe('tests', function describe() {
+  let main;
+  before(async () => {
+    main = new Runtime('main_loop.huff', pathToTestData, true);
+  });
+
+  it('test runs', async () => {
+    const calldata = [
+      { index: 0, value: new bn(1) },
+      { index: 32, value: new bn(2) },
+    ];
+    const initialMem = [];
+    const initialStack = [5, 3];
+    const callvalue = 0;
+
+    const {
+      stack,
+      mem,
+      gas,
+      bytecode,
+      returndata,
+    } = await main('MAIN__SAFEMATH', initialStack, initialMem, calldata, callvalue)
+
+    console.log('stack: ', stack);
+    console.log('mem: ', mem);
+    console.log('gas: ', gas);
+    console.log('bytecode: ', bytecode);
+    console.log('returndata: ', returndata);
+  });
+
+});
